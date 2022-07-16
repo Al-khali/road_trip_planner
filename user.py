fcf2181423a5dc6a182371382214ab507318bd5d
Al-khali <aourik.khalid@hotmail.com>
2022-07-16 17:22:58 +0200

    adding first script

---
+++ user.py
@@ -0,0 +1,22 @@
+
+
+
+def get_location():
+    location = []
+    location = 'not_empty'
+    is_first = True
+    while is_valid(location):
+        if is_first:
+            location = str(input("Enter a starting location: "))
+            if is_valid(location):
+                locations.append(location)
+                is_first = False
+            else:
+                location = str(input("\nEnter location you'd like to visit (enter no or newline to stop entering destinations): "))
+            if is_valid(location):
+                locations.append(location)
+    return locations
+
+
+def is_valid(location):
+    return location != '' and location != 'n' and location != 'No' and location != 'N' and location != 'no'
