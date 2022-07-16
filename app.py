a2c7c5f9134e47fd13794db5be9526d04f83fc84
Al-khali <aourik.khalid@hotmail.com>
2022-07-16 17:32:50 +0200

    adding app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import user
+
+
+api_key = input("Enter your Google API Key containing access to Distance Matrix API: ")
+
+locations = user.get_locations()
+
+
+#  get distence url
+
+
+# parse_response
+
+
+# order visit ?
+
+
+# return response
